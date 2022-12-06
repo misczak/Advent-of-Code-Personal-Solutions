45d53ae2e617f240e8c0b8473dcf6610e93a144a
John Misczak <johnmisczak@gmail.com>
2022-12-06 09:45:57 -0500

    Completed sixth day of Advent of Code 2022

---
+++ 2022/6/puzzle1.py
@@ -0,0 +1,36 @@
+def move_list(list, new_element):
+    # When we need to add a new value, remove the first element and add the new element to the end of the list
+    list.pop(0)
+    list.append(new_element)
+    return list
+
+def check_duplicates(list):
+    # Turn list into a set which removes duplicates, and see if it is the same size as the original list
+    return len(list) != len(set(list))
+
+counter = 0
+datastream = []
+duplicates_exist = True
+
+with open('input.txt') as f:
+    while duplicates_exist:
+        counter += 1
+        # Read one character at a time
+        char = f.read(1)
+
+        if not char:
+            print('Reached end of file')
+            break
+
+        # Boostrap the list with the first four values
+        if counter <= 4:
+            datastream.append(char)
+        else:
+            # After the first four values, start checking for duplicates and moving the list accordingly
+            datastream = move_list(datastream, char)
+            duplicates_exist = check_duplicates(datastream)
+            # else:
+            #     print('Start of Packet found at position ' + str(counter))
+            #     break
+        
+print('Start of packet is found at ' + str(counter))
